537f118fa59fd012b746f733580f1a04094a0082
hakim <h_mokeddem@esi.dz>
2018-05-22 11:59:01 +0100

    Fix MalformedJsonException

--- app/src/main/java/example/android/com/datasynchronization/retrofit/RetrofitService.kt
+++ app/src/main/java/example/android/com/datasynchronization/retrofit/RetrofitService.kt
@@ -2,12 +2,20 @@ package example.android.com.datasynchronization.retrofit
 
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import com.google.gson.GsonBuilder
+import com.google.gson.Gson
+
+
 
 object RetrofitService {
 
+    var gson = GsonBuilder()
+            .setLenient()
+            .create()
+
     val endpoint: Endpoint by lazy {
         Retrofit.Builder().baseUrl(baseUrl).
-                addConverterFactory(GsonConverterFactory.create()).build()
+                addConverterFactory(GsonConverterFactory.create(gson)).build()
                 .create(Endpoint::class.java)
 
     }
